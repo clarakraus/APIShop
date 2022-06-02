589869dc4cace0b0a06f7107660a7f0cb878fca9
Clara Kraus <claramariakrausgmail.com>
2022-06-02 22:08:03 +0200

    Initialize

---
+++ src/main/java/com/example/apishop/ApiShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.apishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apishop/ApiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
